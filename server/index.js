51238d96154e8301dc0268e3b29e50ccc1afa052
rajat-bhatt-au9 <rajatbhatt500@gmail.com>
2021-02-16 13:41:35 +0530

    seprated logic from routes and put in controllers

--- server/index.js
+++ server/index.js
@@ -3,7 +3,11 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import mongoose from "mongoose";
 
+import postRoutes from "./routes/posts.js";
+
 const app = express();
+
+app.use("/posts", postRoutes);
 
 app.use(bodyParser.json({ Limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ Limit: "30mb", extended: true }));
